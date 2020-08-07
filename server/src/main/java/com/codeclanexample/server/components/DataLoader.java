9e3889b7840b7aad3643a0fd32ba890d9a0c7ae4
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-08-07 12:03:02 +0100

    enums update

--- server/src/main/java/com/codeclanexample/server/components/DataLoader.java
+++ server/src/main/java/com/codeclanexample/server/components/DataLoader.java
@@ -1,6 +1,7 @@
 package com.codeclanexample.server.components;
 
 import com.codeclanexample.server.models.Product;
+import com.codeclanexample.server.models.ProductTypes;
 import com.codeclanexample.server.repositories.OrderRepository;
 import com.codeclanexample.server.repositories.ProductRepository;
 import com.codeclanexample.server.repositories.UserRepository;
@@ -34,13 +35,13 @@ public class DataLoader implements ApplicationRunner {
 
         Product rum1 = new Product("Flor de Cana 7yr", 700, 10, 25.00,
                 "Another gem from the Flor de Cana stable. The 7yr has hints of almond and vanilla, with a mild spiceyness",
-                "Nicaraguan", 40.00, "gold", "rum", "http://localhost:8080/api/images/flor_de_cana7yr.jpg");
+                "Nicaraguan", 40.00, "gold", ProductTypes.RUM, "http://localhost:8080/api/images/flor_de_cana7yr.jpg");
         Product rum2 = new Product("O'Haras Spiced Rum", 700, 10, 24.50, "Fine Caribbean rum with added spice!",
-                "Carabbean", 40.00, "spiced", "rum", "http://localhost:8080/api/images/o_haras_spiced_rum.jpg");
+                "Carabbean", 40.00, "spiced", ProductTypes.RUM, "http://localhost:8080/api/images/o_haras_spiced_rum.jpg");
         Product rum3 = new Product("Plantation 3 Star White Rum", 700, 10, 24.00, "Plantation 3 Star White Rum contains a blend of rums from the '3 Stars of the Caribbean', namely Jamaica, Barbados and Trinidad to create this crisp, tasty Plantation rum.",
-                "Caribbean", 40.00, "white", "rum", "http://localhost:8080/api/images/plantation_3_star_rum.jpg");
+                "Caribbean", 40.00, "white", ProductTypes.RUM, "http://localhost:8080/api/images/plantation_3_star_rum.jpg");
         Product rum4 = new Product("Ron Abuelo 7 yr", 700, 10, 22.00, "Ron Abuelo 7 yr is a Panamanian rum made using distillery grown sugar cane and aged in white oak barrels.",
-                "Panama", 40.00, "dark", "rum", "http://localhost:8080/api/images/ron_abuelo_7_years.jpg");
+                "Panama", 40.00, "dark", ProductTypes.RUM, "http://localhost:8080/api/images/ron_abuelo_7_years.jpg");
         Product rum5 = new Product();
 
         Product gin1 = new Product();
