37b6c97c8ba7486d7112c7cb388b50881a5195b7
craiglaws <craiglaws1@gmail.com>
2020-08-10 15:04:36 +0100

    checkout page getting there

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,14 +1,53 @@
-import React from 'react';
-// import {Elements, CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
+import React, {Component} from 'react';
+import {Elements, CardElement, useStripe, useElements} from "@stripe/react-stripe-js";
+import {loadStripe} from "@stripe/stripe-js";
 
-// const stripePromise = loadStripe("pk_test_51HDZiqLGoZTAM9MyFgzd39e6jbuoIQ2sAx9Wu2MjUohN0LwejPINgBueuOtAHaQDmj0oPSyYmxZPkP5nuISPPsfB001GjlRavS");
+const stripePromise = loadStripe("pk_test_51HDZiqLGoZTAM9MyFgzd39e6jbuoIQ2sAx9Wu2MjUohN0LwejPINgBueuOtAHaQDmj0oPSyYmxZPkP5nuISPPsfB001GjlRavS");
 
-const Checkout = () => {
+class Checkout extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      order: {
+        name: "",
+        address: "",
+        age: 0,
+        products: props.cart
+      }
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+
+handleChange(event){
+  let propertyName = event.target.name;
+  let order = this.state.order;
+  order[propertyName] = event.target.value;
+  this.setState({order: order})
+}
+
+handleSubmit(event){
+  event.preventDefault();
+  this.props.placeOrder(this.state.order);
+}
+
+
+
+render(){
   return (
-    <div> </div>
+    <div>
+    <Elements stripe={stripePromise}>
+    <form name="checkout" onSubmit={this.handleSubmit}>
+    <input required type="text" placeholder="Name" name="name" onChange={this.handleChange} value={this.state.order.name}/>
+    <input required type="text" placeholder="Address" name="address" onChange={this.handleChange} value={this.state.order.address}/>
+    <input required type="number" placeholder="Age" name="age" onChange={this.handleChange} value={this.state.order.age}/>
+    <button type="submit">Place Order </button>
+    </form>
+    </Elements>
+    </div>
   )
-
+}
 }
 
 export default Checkout;
--- client/src/helpers/Request.js
+++ client/src/helpers/Request.js
@@ -3,20 +3,25 @@ class Request {
     return fetch(url)
     .then(res => res.json());
   }
-
   delete(url) {
     return fetch(url, {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
     });
   }
-
   post(url, payload) {
     return fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
     });
+  }
+  update(url, payload) {
+    return fetch(url), {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application.json' },
+      body: JSON.stringify(payload)
+    }
   }
 }
 export default Request;
